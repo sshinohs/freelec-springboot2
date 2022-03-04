11f79ae04bd12ca8807ef13e3ede6417ded19b66
sshinohs <sshinohs@gmail.com>
2022-03-04 22:53:03 +0900

    2장 테스트코드 끝

---
+++ src/main/java/com/sshinohs/stock/ml/Application.java
@@ -0,0 +1,11 @@
+package com.sshinohs.stock.ml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
