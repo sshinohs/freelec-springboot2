373dc32c0b2a4ce8c22049e78b87894fb91f4fcf
sshinohs <sshinohs@gmail.com>
2022-03-06 21:32:11 +0900

    3장 JPA를 이용한 데이터베이스 접근 코드 실습 중

---
+++ src/main/java/com/sshinohs/stock/ml/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.sshinohs.stock.ml.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
