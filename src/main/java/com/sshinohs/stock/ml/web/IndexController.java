71922c6182c8c3a07ae2975c2877c0139332d07d
sshinohs <sshinohs@gmail.com>
2022-03-08 11:42:09 +0900

    fixed untracked files

--- src/main/java/com/sshinohs/stock/ml/web/IndexController.java
+++ src/main/java/com/sshinohs/stock/ml/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sshinohs.stock.ml.web;
 
+import com.sshinohs.stock.ml.config.auth.LoginUser;
+import com.sshinohs.stock.ml.config.auth.dto.SessionUser;
 import com.sshinohs.stock.ml.service.PostsService;
 import com.sshinohs.stock.ml.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
